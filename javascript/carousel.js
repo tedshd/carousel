11f8c5a35e8e204167a3cecbbf2cf31c4dc4d7f0
Ted Shiu <tedshd@gmail.com>
2015-11-13 10:53:53 +0800

    [dev] Update not over view count not show arrow.

--- javascript/carousel.js
+++ javascript/carousel.js
@@ -6,9 +6,15 @@
 
 (function () {
 	function carousel (option) {
-		if (!option && !option.dom) {
-			console.error('not set dom');
-			return;
+		if (!option) {
+			if (!option.dom) {
+				console.error('not set dom');
+				return;
+			}
+			if (!option.viewCount) {
+				console.error('not set viewCount');
+				return;
+			}
 		}
 		var dot,
 			dom = option.dom,
@@ -21,14 +27,14 @@
 			slideContent.style.left = '-' + w*option.viewCount*count + 'px';
 		}
 		function slideDot () {
-			document.querySelectorAll('.dot')[currentCount].setAttribute('class', 'dot');
+			document.querySelectorAll('.dot')[currentCount].setAttribute('data-state', '');
 			currentCount = parseInt(this.getAttribute('data-count'), 10);
-			document.querySelectorAll('.dot')[currentCount].setAttribute('class', 'dot focus');
+			document.querySelectorAll('.dot')[currentCount].setAttribute('data-state', 'focus');
 			slide(currentCount);
 		}
 		function slideArrow () {
 			var arrow = this.getAttribute('class');
-			document.querySelectorAll('.dot')[currentCount].setAttribute('class', 'dot');
+			document.querySelectorAll('.dot')[currentCount].setAttribute('data-state', '');
 			if (arrow === 'arrow-l') {
 				if (currentCount !== 0) {
 					currentCount--;
@@ -38,29 +44,32 @@
 					currentCount++;
 				}
 			}
-			document.querySelectorAll('.dot')[currentCount].setAttribute('class', 'dot focus');
+			document.querySelectorAll('.dot')[currentCount].setAttribute('data-state', 'focus');
 			slide(currentCount);
 		}
-		dom.querySelectorAll('.arrow-l')[0].onclick = slideArrow;
-		dom.querySelectorAll('.arrow-r')[0].onclick = slideArrow;
-		if (option.viewCount) {
-			dot = Math.ceil(l / option.viewCount);
-			if (dot > 1) {
-				var dotDom = document.createElement('div');
-				dotDom.setAttribute('style', 'text-align:center;');
-				for (var i = 0; i < dot; i++) {
-					var span = document.createElement('span');
-					if (i === 0) {
-						span.setAttribute('class', 'dot focus');
-					} else {
-						span.setAttribute('class', 'dot');
-					}
-					span.setAttribute('data-count', i);
-					span.onclick = slideDot;
-					dotDom.appendChild(span);
+		dot = Math.ceil(l / option.viewCount);
+		dom.querySelectorAll('.product-interest')[0].style.width = w*l + 'px';
+		if (dot > 1) {
+			var arrowL = dom.querySelectorAll('.arrow-l')[0],
+				arrowR = dom.querySelectorAll('.arrow-r')[0];
+			arrowL.onclick = slideArrow;
+			arrowR.onclick = slideArrow;
+			arrowL.setAttribute('data-state', '');
+			arrowR.setAttribute('data-state', '');
+
+			var dotDom = document.createElement('div');
+			dotDom.setAttribute('style', 'text-align:center;');
+			for (var i = 0; i < dot; i++) {
+				var span = document.createElement('span');
+				if (i === 0) {
+					span.setAttribute('data-state', 'focus');
 				}
-				dom.appendChild(dotDom);
+				span.setAttribute('class', 'dot');
+				span.setAttribute('data-count', i);
+				span.onclick = slideDot;
+				dotDom.appendChild(span);
 			}
+			dom.appendChild(dotDom);
 		}
 	}
 	this.carousel = carousel;
