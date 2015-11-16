a2e91e330958fb558d14ae2f32ac03d777eb3357
Ted Shiu <tedshd@gmail.com>
2015-11-16 12:03:32 +0800

    [dev] Add mobile touch swipe.

--- javascript/carousel.js
+++ javascript/carousel.js
@@ -71,6 +71,62 @@
 			}
 			dom.appendChild(dotDom);
 		}
+
+		dom.addEventListener('touchstart', handleTouchStart, false);
+		dom.addEventListener('touchmove', handleTouchMove, false);
+
+		var xDown = null;
+		var yDown = null;
+
+		function handleTouchStart(evt) {
+		    xDown = evt.touches[0].clientX;
+		    yDown = evt.touches[0].clientY;
+		}
+
+		function handleTouchMove(evt) {
+		    if ( ! xDown || ! yDown ) {
+		        return;
+		    }
+
+		    var xUp = evt.touches[0].clientX;
+		    var yUp = evt.touches[0].clientY;
+
+		    var xDiff = xDown - xUp;
+		    var yDiff = yDown - yUp;
+
+		    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+		        if ( xDiff > 0 ) {
+		            /* left swipe */
+		            console.log('left swipe');
+		            document.querySelectorAll('.dot')[currentCount].setAttribute('data-state', '');
+					if ((Math.ceil(l / option.viewCount) - 1) > currentCount) {
+						currentCount++;
+					}
+					document.querySelectorAll('.dot')[currentCount].setAttribute('data-state', 'focus');
+					slide(currentCount);
+		        } else {
+		            /* right swipe */
+		            console.log('right swipe');
+		            document.querySelectorAll('.dot')[currentCount].setAttribute('data-state', '');
+					if (currentCount !== 0) {
+						currentCount--;
+					}
+					document.querySelectorAll('.dot')[currentCount].setAttribute('data-state', 'focus');
+					slide(currentCount);
+		        }
+		    } else {
+		        if ( yDiff > 0 ) {
+		            /* up swipe */
+		            console.log('up swipe');
+		        } else {
+		            /* down swipe */
+		            console.log('down swipe');
+		        }
+		    }
+		    /* reset values */
+		    xDown = null;
+		    yDown = null;
+		}
 	}
 	this.carousel = carousel;
 })();
